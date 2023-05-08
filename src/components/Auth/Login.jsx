e33aa3daafc6ea7877a95b9e14fd6ea4993cf08e
KoushikDutta1999 <iamkoushik1999@gmail.com>
2023-05-08 23:16:06 +0530

    #41 Implementing Redux Toolkit

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -8,16 +8,25 @@ import {
   Input,
   VStack,
 } from '@chakra-ui/react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { login } from '../../redux/user';
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  const dispatch = useDispatch();
+
+  const submitHandler = e => {
+    e.preventDefault();
+    dispatch(login(email, password));
+  };
   return (
     <Container h={'95vh'}>
       <VStack h={'full'} justifyContent={'center'} spacing={'16'}>
         <Heading children={'Welcome to CourseBundler'} />
-        <form style={{ width: '100%' }}>
+        <form onSubmit={submitHandler} style={{ width: '100%' }}>
           <Box my={'4'}>
             <FormLabel htmlFor="email" children="Email" />
             <Input
